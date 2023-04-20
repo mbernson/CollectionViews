6baba2ffc1a6ee7bc1ec2e2eef238d614e2aec3f
Mathijs Bernson <mathijsb@q42.nl>
2023-04-20 13:07:15 +0200

    Basic CollectionView setup using diffable datasource, configurations and compositional layout

--- CollectionViews/ViewController.swift
+++ CollectionViews/ViewController.swift
@@ -6,14 +6,59 @@
 //
 
 import UIKit
+import SwiftUI
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
+
+    typealias ItemID = String
+    typealias SectionID = Int
+
+    // We need to keep a strong reference to the data source, otherwise it will be deallocated
+    private var dataSource: UICollectionViewDiffableDataSource<SectionID, ItemID>!
+
+    init() {
+        let layout = UICollectionViewCompositionalLayout { sectionIndex, layoutEnvironment in
+            // Define the layout for each section in the collection view.
+            // This example uses a single section with a simple list layout.
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(44))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            let group = NSCollectionLayoutGroup.vertical(layoutSize: itemSize, subitems: [item])
+            let section = NSCollectionLayoutSection(group: group)
+            return section
+        }
+
+        super.init(collectionViewLayout: layout)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        title = "CollectionView demo"
+
+        let registration = UICollectionView.CellRegistration<UICollectionViewCell, ItemID> { cell, indexPath, identifier in
+            cell.contentConfiguration = UIHostingConfiguration {
+                HStack {
+                    Image(systemName: "star").foregroundStyle(.purple)
+                    Text(identifier)
+                    Spacer()
+                }
+            }
+        }
+
+        let dataSource = UICollectionViewDiffableDataSource<SectionID, ItemID>(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            return collectionView.dequeueConfiguredReusableCell(using: registration, for: indexPath, item: itemIdentifier)
+        })
+        self.dataSource = dataSource
+        self.collectionView.dataSource = dataSource
+
+        // Populate the collection view with some initial data.
+        var snapshot = NSDiffableDataSourceSnapshot<SectionID, ItemID>()
+        snapshot.appendSections([0])
+        snapshot.appendItems(["Item 1", "Item 2", "Item 3"], toSection: 0)
+        dataSource.apply(snapshot, animatingDifferences: false)
     }
-
-
 }
-
